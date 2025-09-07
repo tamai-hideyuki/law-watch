f8cfa32829846ce4905a48acce11749eb1693ab3
tamai-hideyuki <hide27yuki@gmail.com>
2025-09-07 13:21:25 +0900

    molecules の SearchForm ができた

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
